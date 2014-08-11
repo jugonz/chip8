dc2f0ab358eba3168c6e20b17e4f268dc4bde952
Julian Gonzalez <jugonz97@mit.edu>
2014-08-10 22:26:28 -0400

    Removed some debug statements from main

--- src/jugonz/chip8/src/main/main.go
+++ src/jugonz/chip8/src/main/main.go
@@ -7,16 +7,13 @@ import (
 var chip8 arch.Arch
 
 func main() {
-	count := 0
 	chip8 = arch.MakeChip8(true) // DEBUG on.
 	chip8.LoadGame("../../c8games/PONG2")
 
 	for !chip8.ShouldClose() {
-		count++
 		chip8.EmulateCycle()
 
 		chip8.DrawScreen() // Only draws if needed.
-
 		chip8.SetKeys()
 	}
 
