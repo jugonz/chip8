b54a59f89cffc8a59182377a2a15291192e19b54
Julian Gonzalez <jugonz97@mit.edu>
2014-08-23 11:35:51 -0400

    Added debug and file path flags.
    Now debug and the path to chip8 games are not harcoded.
    Hooray!

--- src/jugonz/chip8/src/main/main.go
+++ src/jugonz/chip8/src/main/main.go
@@ -1,17 +1,27 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"jugonz/chip8/src/arch"
 	"runtime"
 )
 
+var path = flag.String("path", "", "path to a Chip8 ROM")
+var debug = flag.Bool("debug", false, "debug mode")
 var chip8 arch.Arch
 
 func main() {
-	runtime.LockOSThread()       // OpenGL requires code to be run on main thread.
-	chip8 = arch.MakeChip8(true) // DEBUG on.
+	flag.Parse()
+	if *path == "" {
+		fmt.Printf("No chip8 file path provided, quitting!\n")
+		return
+	}
 
-	chip8.LoadGame("../../c8games/PONG2")
+	runtime.LockOSThread()         // OpenGL requires code to be run on main thread.
+	chip8 = arch.MakeChip8(*debug) // DEBUG on.
+
+	chip8.LoadGame(*path)
 
 	chip8.Run() // Terminates when the quit key is pressed.
 
